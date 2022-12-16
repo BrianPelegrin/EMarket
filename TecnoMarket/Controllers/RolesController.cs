32b848937dbb349a33c315fb60d70d9d1a19cef0
Miguel Cruz <miguelangelcruz200217@gmail.com>
2022-12-15 23:09:46 -0400

    ROLES IMPLEMENTATION

---
+++ TecnoMarket/Controllers/RolesController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TecnoMarket.Core.Entities;
+using TecnoMarket.Extensions;
+
+namespace TecnoMarket.Controllers
+{
+    [Authorize]
+    public class RolesController : BaseController<Product, RolesController>
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        // OBTENER LOS ROLES.
+        [HttpGet]
+        public IActionResult RolesList()
+        {
+            BasicModal("RolesAdd", "Formulario de Roles");
+            var query = _roleManager.Roles.ToList();
+            return View(query);
+        }
+
+        // VISTA PARA AGREGAR ROLES.
+        [HttpGet]
+        public IActionResult RolesAdd()
+        {
+            return View();
+        }
+
+        // ENVIAR ROLES.
+        [HttpPost]
+        public IActionResult RolesAdd(IdentityRole role)
+        {
+            return View(role);
+        }
+
+        // VISTA PARA ACTUALIZAR ROLES.
+        [HttpGet]
+        public IActionResult RolesUpdate()
+        {
+            return View();
+        }
+
+        // ACTUALIZAR ROL.
+        [HttpPost]
+        public IActionResult RolesUpdate(IdentityRole role)
+        {
+            return View(role);
+        }
+
+        // ELIMINAR ROL.
+        [HttpPost]
+        public IActionResult RolesDelete(IdentityRole role)
+        {
+            return View(role);
+        }
+
+    }
+}
